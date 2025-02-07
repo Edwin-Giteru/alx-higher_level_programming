1c5a1feff78171e2f4d1795cd21ee3f3bdd3433e
Edwin-Giteru <egiteru5@gmail.com>
2025-02-07 19:16:31 +0300

    JS_web_query

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const myHeaderColor = document.query("header");
+	myHeaderColor.style.color = '#FF0000';
+});
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+$('#toggle_header').click(() => {
+	$('header').toggleClass('red green');
+});
